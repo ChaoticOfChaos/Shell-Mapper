073fc3b57dd38d947e1d02b8a885fb5fe9e447f2
ChaoticOfChaos <143908924+ChaoticOfChaos@users.noreply.github.com>
2025-06-01 02:09:49 -0300

    Add files via upload

---
+++ ShellMapper.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set /p base_ip="IP Base > "
+set /p sider="Sider > "
+
+if %sider% GEQ 32 (
+    ping -n 1 %base_ip%
+) else (
+    if %sider% GEQ 24 (
+        for /L %%i in (1,1,254) do (
+            set ip=%base_ip%.%%i
+            ping -n 1 !ip! > nul
+            if !errorlevel! EQU 0 (
+                echo !ip! is UP
+            )
+        )
+    ) else (
+        echo "Error Sider Not Supported :("
+    )
+)
---
+++ shell_mapper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+read -p "IP Base > " ip_base
+read -p "Sider > " sider
+
+#
+
+if [ "$sider" -eq 32 ]; then
+    ping -c 1 $ip_base
+
+elif [ "$sider" -eq 24 ]; then
+    for i in $(seq 1 254); do
+        ip="$ip_base.$i"
+        ping -c 1 $ip > /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo "$ip is UP"
+        fi
+    done
+else
+    echo "Error Sider Not Supported :("
+
+fi
